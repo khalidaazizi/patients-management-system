c1e5d0d64cce5cab75127d22de3c42b61835d1c7
khalidaazizi <khalidaazizi092@gmail.com>
2026-01-01 09:03:07 +0430

    connect  api

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://127.0.0.1:8000/api", // آدرس بک‌اند لاراول
+  // baseURL: "http://127.0.0.1:8000/api", // آدرس بک‌اند لاراول
+  baseURL: "https://doctorsystem-production.up.railway.app/api", // آدرس بک‌اند لاراول
 });
 
 export default api;
